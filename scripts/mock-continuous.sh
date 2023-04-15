29ca8f65c1790a4f7d7dd3d16722a617c52a2f93
stochosclub <112891660+stochosclub@users.noreply.github.com>
2023-04-15 17:09:01 +0200

    b

--- scripts/mock-continuous.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-while true; do
-  random_string=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 64 | head -n 1)
-  echo "$random_string"
-  sleep 0.2
-done
--- scripts/mock-user-input.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo This is a mock input script
-echo It is used to test handling input from the user
-echo Input: 
-read number
-echo You entered $number
