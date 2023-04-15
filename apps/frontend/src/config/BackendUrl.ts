0c53aa21ec63f38309208cc5ef300ee1edcc451b
stochosclub <112891660+stochosclub@users.noreply.github.com>
2023-04-15 19:34:28 +0200

    a

--- apps/frontend/src/config/BackendUrl.ts
+++ apps/frontend/src/config/BackendUrl.ts
@@ -1,7 +1,7 @@
 declare const process: any;
 
 function getBackendUrlHost() {
-  const defaultUrl = 'http://localhost:2200';
+  const defaultUrl = 'https://agentgpt-api-ale-ok.onrender.com:2200';
   const envUrl = process?.env?.BACKEND_URL || '';
   const localStorageUrl = localStorage.getItem('backendUrl') || '';
   const urlParam = new URLSearchParams(window.location.search).get('api') || '';
